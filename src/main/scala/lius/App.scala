a6bff4a4fbb048b7ba9417c32dddd95ab3688f5d
刘帅 <liushuai@chuchujie.com>
2016-08-10 16:25:03 +0800

    fix

--- src/main/scala/lius/App.scala
+++ src/main/scala/lius/App.scala
@@ -1,6 +1,6 @@
 package lius
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{Actor,ActorSystem, Props}
 import akka.pattern.ask
 import akka.util.Timeout
 import scala.concurrent.duration._
@@ -9,19 +9,32 @@ import akka.dispatch.ExecutionContexts._
 /**
   * Created by Administrator on 2016/8/9.
   */
-object App {
-//  implicit val ec = global
+object App{
+  //  implicit val ec = global
   def main(args : Array[String]): Unit ={
-    val system = ActorSystem("system")
+    val system = ActorSystem("wordCount_system")
     val actor = system.actorOf(Props(new WordCounterActor("C:\\Users\\Administrator\\Desktop\\aaa.txt")))
     actor ! startProcessFileMsg()
-
     Thread.sleep(2000)
-
     system.shutdown
-//    future.map{ result => {
-//      println("totoal words in file" + args(0) + "is :" + result)
-//      system.shutdown}
-//    }
+
+    //采用同步方式
+    //    val future = actor ? startProcessFileMsg()
+    //    future.map{ result => {
+    //      println("totoal words in file" + args(0) + "is :" + result)
+    //      system.shutdown}
+    //    }
+  }
+}
+
+
+class App extends Actor{
+  def receive = {
+    case  resultMsg(result) => {
+      println(result)
+    }
+    case _ => {
+      println("error:message not recognized!")
+    }
   }
 }
