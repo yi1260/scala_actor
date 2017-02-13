22de96d81c4cce6e2f61a974a05d175ad6e2fadd
刘帅 <liushuai@chuchujie.com>
2017-02-13 12:09:58 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-MODULE=$1
-ARTIFACT_ID=${MODULE}
 
 HOME_DIR=$(pushd $(dirname $0)/ >/dev/null && pwd -P && popd >/dev/null)
-
-cd ${HOME_DIR}/${MODULE}
 
 mvn assembly:assembly && {
   cd ..
   mkdir lib
-  cp ${HOME_DIR}/${MODULE}/target/${ARTIFACT_ID}-1.0-SNAPSHOT-allinone.jar ${HOME_DIR}/lib/${ARTIFACT_ID}-1-0-SNAPSHOT-allinone.jar
+  cp ${HOME_DIR}/target/test-scala-akka-1.0-SNAPSHOT-allinone.jar ${HOME_DIR}/lib/test-scala-akka-1-0-SNAPSHOT-allinone.jar
 } || {
   echo "Build failed"
   exit 255
